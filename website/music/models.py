2d35a4ab7637d9ef61ae56cac1686203f288365b
shivshankarkeshari <shiv.s.keshari@gmail.com>
2019-11-29 19:55:25 +0530

    __str__

--- website/music/models.py
+++ website/music/models.py
@@ -6,6 +6,8 @@ class Album(models.Model):
     album_title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     album_logo= models.CharField(max_length=100)
+    def __str__(self):
+        return self.album_title + " - " + self.artist
 
 class song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
